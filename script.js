dbcea996107563ea788246aa8591ce1fca669d1b
Gopal Roy <105766337+gopalroy1@users.noreply.github.com>
2023-04-13 11:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(sampleObject!=undefined){
+		return true;
+	}
+  return false;
 }
 
 // Do not change the code below
+console.log("hello world")
 const key = prompt("Enter Key.");
 alert(hasKey(key));
